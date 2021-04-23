727a5e70ada0bef3620cf45212c79e31dd4a29a1
msrfjfj <mtsales1337@gmail.com>
2021-04-22 22:19:31 -0300

    Inclusão de Models e Repositories

--- src/routes/appointments.routes.ts
+++ src/routes/appointments.routes.ts
@@ -1,20 +1,30 @@
-import {request, response, Router} from 'express';
-import {uuid} from 'uuidv4';
+import { Router } from 'express';
+import { startOfHour, parseISO } from 'date-fns';
+import AppointmentsRepository from '../repositories/AppointmentsRepository';
 
 const appoinstmentsRouter = Router();
+const appointmentsRepository = new AppointmentsRepository();
 
-const appointments=[];
+appoinstmentsRouter.get('/',(request,response) =>{
 
-appoinstmentsRouter.post('/',(request,response)=>{
-    const {provider,date} = request.body;
+  const appointments = appointmentsRepository.All();
+  
+  return response.json(appointments);
 
-    const appointment = {
-        id:uuid(),
-        provider,
-        date
-    }
-    appointments.push(appointment)
-    return response.json(appointment);
 });
 
-export default appoinstmentsRouter ;
+appoinstmentsRouter.post('/', (request, response) => {
+  const { provider, date } = request.body;
+
+  const parsedDate = startOfHour(parseISO(date));
+
+  const findAppointmentInSameDate = appointmentsRepository.findByDate(parsedDate);
+
+  if (findAppointmentInSameDate) {
+    return response.status(400).json({ message: 'This appointment is already booked' });
+  }
+  const appointment = appointmentsRepository.create(provider, parsedDate);
+  return response.json(appointment);
+});
+
+export default appoinstmentsRouter;
